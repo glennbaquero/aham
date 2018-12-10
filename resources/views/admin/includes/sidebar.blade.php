108d4f3a99cfebc674bc768be99e90ce25b4a8aa
Matthew Molina <mattiyahu11@gmail.com>
2018-12-10 16:18:52 +0800

    #18 Fixed Sidebar and added admin type field on crud

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,6 +5,18 @@
     <ul class="sidebar-menu" data-widget="tree">
       <li class="header">Warranty</li>
       <li><a href=""><i class="fa fa-book"></i> <span>Application</span></a></li>
+      <li class="treeview {{ $checker->route->isActive(['admin.request'], 'menu-open') }}">
+        <a href="#">
+          <i class="fas fa-toolbox"></i> <span> Repair Service</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu {{ $checker->route->isActive('admin.request') }}">
+          <li class="{{ $checker->route->isActive('admin.request') }}"><a href="{{ route('admin.request') }}"><i class="fas fa-hammer"></i> Repair Request</a></li>
+          <li><a href=""><i class="fas fa-history"></i> History</a></li>
+        </ul>
+      </li>
       
       @if ($checker->permission->can(['admin.product.edit', 'admin.product.create', 'admin.product.destroy']))
         <li class="header">Products</li>
@@ -30,14 +42,14 @@
       @endif
 
       <li class="header">Content Management</li>
-      <li class="treeview {{ $checker->route->isActive(['admin.pages.', 'admin.page-items.'], 'menu-open') }}">
+      <li class="treeview {{ $checker->route->isActive(['admin.pages.', 'admin.page-items.', 'admin.carousel.'], 'menu-open') }}">
         <a href="#">
           <i class="fas fa-puzzle-piece"></i> <span>Page Management</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
-        <ul class="treeview-menu {{ $checker->route->isActive(['admin.pages.', 'admin.page-items.']) }}">
+        <ul class="treeview-menu {{ $checker->route->isActive(['admin.pages.', 'admin.page-items.', 'admin.carousel.']) }}">
           <li class="{{ $checker->route->isActive('admin.pages.') }}">
             <a href="{{ route('admin.pages.index') }}"><i class="fas fa-boxes"></i> Page</a>
           </li>
@@ -49,21 +61,6 @@
           </li>
         </ul>
       </li>
-      <li class="treeview">
-          <a href="#">
-            <i class="fas fa-toolbox"></i> <span> Repair Service</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu {{ $checker->route->isActive('admin.request', 'menu-open') }}">
-            <li class="{{ $checker->route->isActive('admin.request') }}"><a href="{{ route('admin.request') }}"><i class="fas fa-hammer"></i> Repair Request</a></li>
-            <li><a href=""><i class="fas fa-history"></i> History</a></li>
-          </ul>
-        </li>
-        <li><a href=""><i class="fa fa-book"></i> <span>Repair Man</span></a></li>
-      <li><a href=""><i class="fa fa-book"></i> <span>Application</span></a></li>
-      <li class="active treeview menu-open">
 
       <li class="header">Security</li>
       <li class="treeview {{ $checker->route->isActive(['admin.administrator', 'admin.role'], 'menu-open') }}">
