90085e593ecae83ea5263d89144e399c6fa3697d
Matthew Molina <mattiyahu11@gmail.com>
2018-12-11 14:13:39 +0800

    #61 Resolve "Product Seeder and Factory"

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,8 +2,20 @@
 
 use Faker\Generator as Faker;
 
+use App\Helpers;
+
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'category_id' => function() {
+        	return Helpers::randomOrCreate(App\Category::class)->id;
+        },
+		'type_id' => function() {
+			return Helpers::randomOrCreate(App\Type::class)->id;
+		},
+		'model' => $faker->unique()->word,
+		'name' => $faker->word,
+		'description' => $faker->paragraph,
+		'specification' => $faker->paragraph,
+		'extended_amount' => $faker->randomNumber(4),
     ];
 });
--- database/seeds/SampleProductSeeder.php
+++ database/seeds/SampleProductSeeder.php
@@ -14,79 +14,11 @@ class SampleProductSeeder extends Seeder
      */
     public function run()
     {
-
         \DB::table('products')->delete();
         \DB::table('product_images')->delete();
 
-        $products = [
-        	[
-        		'category_id' => 1,
-        		'type_id' => 1,
-        		'model' => 'ASSC123',
-        		'name' => 'COOKING MACHINE',
-        		'description' => 'none',
-        		'specification' => 'none',
-        		'extended_amount' => '1200.00',
-        		'image' => [
-        			'product_image' => 'product/njCLJlWlXDnNk6S0ExP2Hl74VtTZ0NmmYTetjvZC.jpeg'
-        		]
-        	],
-        	[
-        		'category_id' => 1,
-        		'type_id' => 2,
-        		'model' => 'XCC444',
-        		'name' => 'IRON MACHINE',
-        		'description' => 'none',
-        		'specification' => 'none',
-        		'extended_amount' => '1200.00',
-        		'image' => [
-        			'product_image' => 'product-images/de7R4KF3WaoDZH72fQJstrYb3W1c3xyjy7mAkIWb.jpeg'
-        		]
-        	],
-        	[
-        		'category_id' => 2,
-        		'type_id' => 1,
-        		'model' => 'IM2334',
-        		'name' => 'IRON MAN',
-        		'description' => 'none',
-        		'specification' => 'none',
-        		'extended_amount' => '1200.00',
-        		'image' => [
-        			'product_image' => 'product/DtU9X99Ht3zGsyvNBq1UOCQiWOfceid9pVDVM5O0.jpeg'
-        		]
-        	],
-        	[
-        		'category_id' => 2,
-        		'type_id' => 3,
-        		'model' => 'ASSC123',
-        		'name' => 'WAR MACHINE',
-        		'description' => 'none',
-        		'specification' => 'none',
-        		'extended_amount' => '1200.00',
-        		'image' => [
-        			'product_image' => 'product-images/de7R4KF3WaoDZH72fQJstrYb3W1c3xyjy7mAkIWb.jpeg'
-        		]
-        	],
-        ];
-
-
-        foreach ($products as $product) {
-        	$data = Product::create([
-        		'category_id' => $product['category_id'],
-        		'type_id' => $product['type_id'],
-        		'model' => $product['model'],
-        		'name' => $product['name'],
-        		'description' => $product['description'],
-        		'specification' => $product['specification'],
-        		'extended_amount' => $product['extended_amount']
-        	]);
-
-        	foreach($product['image'] as $image) {
-        		ProductImage::create([
-        			'product_id' => $data->id,
-        			'image' => $image
-        		]);
-        	}
-        }
+        factory(Product::class, 30)->create()->each(function($item) {
+            $item->images()->saveMany(factory(ProductImage::class, 3)->create());
+        });
     }
 }
