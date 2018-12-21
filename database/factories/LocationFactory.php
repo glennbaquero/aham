8b9e96f8176dfcb12377b5bbfe92138b4eed5537
Matthew Molina <mattiyahu11@gmail.com>
2018-12-21 11:59:12 +0800

    Fixed Location Factory

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Location::class, function (Faker $faker) {
     return [
+        'name' => $faker->streetName,
         'address' => $faker->address,
         'contacts' => [
         	$faker->phoneNumber,
