d0d73a1228f9d912acb34c66f05c50664d190b08
Glenn Carlo Baquero <glenncarlo123@gmail.com>
2018-12-21 11:05:45 +0800

    Fix Issue #90

---
+++ app/Notifications/AssignedTechnicianNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AssignedTechnicianNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject(config('app.name') . ': Assigned Repair Request')
+                    ->greeting('Greetings ' . $notifiable->renderFullName() . ',')
+                    ->line('This is to inform you that you\'ve been assign to repair')
+                    ->action('View Repair', route('admin.discounts'))
+                    ->line('Thank you!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
