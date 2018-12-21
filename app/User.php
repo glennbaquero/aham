10ca5f64c55229898187037b1e211a9cb3b8bc6f
Glenn Carlo Baquero <glenn@praxxys.ph>
2018-12-21 08:34:22 +0800

    Resolve "Notification of invoice paid"

--- app/User.php
+++ app/User.php
@@ -87,8 +87,7 @@ class User extends Authenticatable
     /*
     *  Renderers
     */
-
-
+   
     public function renderFullName() {
         return $this->firstname . ' '. $this->lastname;
     }
