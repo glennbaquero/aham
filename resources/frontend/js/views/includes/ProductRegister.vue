ae273a214292ce2f33bf009bfa9bf2c879af6fe2
Glenn Carlo Baquero <glenncarlo123@gmail.com>
2019-01-18 15:30:11 +0800

    Merge

--- resources/frontend/js/views/includes/ProductRegister.vue
+++ resources/frontend/js/views/includes/ProductRegister.vue
@@ -4,21 +4,17 @@
 			<p class="rw__title">Not yet registered to a warranty plan?</p>
 			<form class="rw__form">
 				<div class="rw__form-row">
-					<select class="select" v-model="product">
+					<select class="select" v-model="product" @change="validate(product[1])">
 						<option disabled selected>Model Number</option>
-						<option v-for="product in products" :value="product.id"> {{ product.model }}</option>
-					</select><div class="tool-tip register" data-tooltip-title="Content here" data-tooltip-position="right"><i class="color--red fa fa-exclamation-circle"></i></div>
+						<option v-for="product in products" :value="[product.id, product.extended_amount]" > {{ product.model }}</option>
+					</select><div class="tool-tip register" data-tooltip-title="This product is not available for Extended Warranty" data-tooltip-position="right" v-show="show"><i class="color--red fa fa-exclamation-circle"></i></div>
 				</div>
 				
 				<div class="rw__form-row">
 					<div class="button">
-						<a class="btn btn-white" @click="redirect(product)"><p>Basic Warranty</p></a
-						><a class="btn outline--white" @click="redirect(product, 1)">
+						<a class="btn btn-white" @click="redirect(product[0])"><p>Basic Warranty</p></a
+						><a class="btn outline--white" @click="redirect(product[0], 1)">
 							<div class="btn-img--1"><img class="img-fit" src="storage/logo-white.png"><p style="margin-left: 90%">Extended Warranty</p></div></a>
-						<!-- <a class="btn btn-blue" @click="redirect(product)"><p>Basic Warranty</p></a>
-						<a class="rw__btn btn outline--blue inlineBlock-parent relative" @click="redirect(product, 1)">
-							<div class="btn-img--1"><img class="img-fit" src="storage/logo-white.png"></div><p>Extended Warranty</p>
-						</a> -->
 					</div>
 				</div>
 			</form>
@@ -29,14 +25,18 @@
 <script>
 	export default {
 		props: {
-			products: Array,
+			products: {
+				id: null,
+				extended_amount: null
+			},
 			extendedurl: String,
 			basicurl: String
 		},
 
 		data() {
 			return {
-				product: null
+				product: null,
+				show:false
 			}
 		},
 
@@ -45,6 +45,14 @@
 		},
 
 		methods: {
+			validate(extended) {
+				if(!extended) {
+					this.show = true;
+				} else {
+					this.show = false;
+				}
+			},
+
 			redirect(id, extended) {
 				if(!id) {
 					return;
--- resources/frontend/js/views/includes/RegisterProduct.vue
+++ resources/frontend/js/views/includes/RegisterProduct.vue
@@ -3,14 +3,14 @@
 		<div class="h__reg-container animate-up">
 			<p class="h__title">Register your product now!</p>
 			<p class="h__desc">Register your product to 1 year free basic warranty or try our extended warranty plan! We've got you covered!</p>
-			<select class="select" v-model="product">
+			<select class="select" v-model="product" @change="validate(product[1])">
 				<option disabled selected>Model Number</option>
-				<option v-for="product in products" :value="product.id"> {{ product.model }}</option>
+				<option v-for="product in products" :value="[product.id, product.extended_amount]"> {{ product.model }}</option>
 			</select>
-			<div class="tool-tip home" data-tooltip-title="Content here" data-tooltip-position="right"><i class="color--red fa fa-exclamation-circle"></i></div>
+			<div class="tool-tip home" data-tooltip-title="Content here" data-tooltip-position="right" v-show="show"><i class="color--red fa fa-exclamation-circle"></i></div>
 			<div class="button">
-				<a class="btn btn-blue" @click="redirect(product)"><p>Basic Warranty</p></a>
-				<a class="rw__btn btn outline--blue inlineBlock-parent relative" @click="redirect(product, 1)">
+				<a class="btn btn-blue" @click="redirect(product[0])"><p>Basic Warranty</p></a>
+				<a class="rw__btn btn outline--blue inlineBlock-parent relative" @click="redirect(product[0], 1)">
 					<div class="btn-img--1"><img class="img-fit" src="storage/logo-white.png"></div><p>Extended Warranty</p>
 				</a>
 			</div>
@@ -27,7 +27,8 @@
 
 		data() {
 			return {
-				product: null
+				product: null,
+				show: false
 			}
 		},
 
@@ -36,6 +37,14 @@
 		},
 
 		methods: {
+			validate(extended) {
+				if(!extended) {
+					this.show = true;
+				} else {
+					this.show = false;
+				}
+			},
+			
 			redirect(id, extended) {
 				console.log(extended);
 				if(!id) {
