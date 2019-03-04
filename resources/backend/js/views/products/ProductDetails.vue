5834f879c52085b8782e95210df7bad811eda7ce
Glenn Carlo Baquero <glenncarlo123@gmail.com>
2019-03-04 08:12:05 +0800

    Fix Issue in CKEditor

--- resources/backend/js/views/products/ProductDetails.vue
+++ resources/backend/js/views/products/ProductDetails.vue
@@ -201,8 +201,10 @@ export default {
     		if (this.model) {
     			this.item = this.model ? this.model : {};
     		}
-
-            this.ckeditor.init();
+            var $this = this;
+            this.$nextTick( () =>  {
+                $this.ckeditor.init();
+            });
             this.dropzone.init();
     	},
 
@@ -212,18 +214,20 @@ export default {
 
     	fetch() {
             this.load(true);
-
-    		axios.post(this.fetchurl)
-    		.then(response => {
-                const data = response.data;
-                this.item = data.item ? data.item : {};
-    		}).catch(error => {
-                console.log(error);
-    		}).then(() => {
-                this.load(false);
-                this.flatpickr.init('.flatpickr', true);
-                this.select2.init('.select2');
-            });
+            // this.$nextTick( () =>  {
+                axios.post(this.fetchurl)
+                    .then(response => {
+                        const data = response.data;
+                        this.item = data.item ? data.item : {};
+                    }).catch(error => {
+                        console.log(error);
+                    }).then(() => {
+                        this.load(false);
+                        this.flatpickr.init('.flatpickr', true);
+                        this.select2.init('.select2');
+                    });
+            // })
+    		
     	},
 
         load(value) {
