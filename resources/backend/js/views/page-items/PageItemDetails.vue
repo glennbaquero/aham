2f714da0791c0dd76d7488987e6951f245feed39
Glenn Carlo <baqueroglenncarlo@gmail.com>
2020-02-14 15:19:16 +0800

    Fix issue in page items

--- resources/backend/js/views/page-items/PageItemDetails.vue
+++ resources/backend/js/views/page-items/PageItemDetails.vue
@@ -68,7 +68,7 @@
 
                                 <div v-show="isContent" class="form-group">
                                     <label>Content</label>
-                                    <textarea name="content" class="content form-control input-sm" placeholder="Content" v-model="item.content">{{ item.content }}</textarea>
+                                    <textarea name="content" class="content form-control input-sm" placeholder="Content" >{{ item.content }}</textarea>
                                 </div>
                             </div>
 
