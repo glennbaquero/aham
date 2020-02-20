09d2305a2098e45475e2bb16a3a22a468e5076f2
Glenn Carlo <baqueroglenncarlo@gmail.com>
2020-02-20 17:00:04 +0800

    Fix issue in about page

--- resources/backend/js/views/page-items/PageItemDetails.vue
+++ resources/backend/js/views/page-items/PageItemDetails.vue
@@ -68,7 +68,7 @@
 
                                 <div v-show="isContent" class="form-group">
                                     <label>Content</label>
-                                    <vue-ckeditor type="classic" v-model="item.content" :editors="editors"></vue-ckeditor>
+                                    <vue-ckeditor v-if="isContent" type="classic" v-model="item.content" :editors="editors"></vue-ckeditor>
                                     <input v-if="isContent"  v-model="item.content" name="content" type="hidden" class="form-control input-sm" placeholder="Content">
                                     <!-- <textarea name="content" class="content form-control input-sm" placeholder="Content" >{{ item.content }}</textarea> -->
                                 </div>
