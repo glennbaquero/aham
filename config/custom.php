0e43cf2a4f2aabec3cfc2993ae7f76aa65a4d0d0
Glenn Carlo Baquero <glenn@praxxys.ph>
2020-07-30 08:32:28 +0800

    Fix issue in location

--- config/custom.php
+++ config/custom.php
@@ -2,6 +2,7 @@
 
 return [
 	'gmap' => [
-		'key' => 'AIzaSyDDreyHA4tJeymR4xD5GAqEKQSRmOd3s3Y',
+		// 'key' => 'AIzaSyDDreyHA4tJeymR4xD5GAqEKQSRmOd3s3Y',
+		'key' => 'AIzaSyAGOOSi2DNHNshDJHT-U8P4QJRglLW4as0',
 	],
 ];
