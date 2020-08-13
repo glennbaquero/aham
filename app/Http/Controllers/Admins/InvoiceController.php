a940d645c987dd973dfe645f7d0f6a5c0d6e8c66
Glenn Carlo Baquero <glenn@praxxys.ph>
2020-08-13 08:32:41 +0800

    Fix issue in index

--- app/Http/Controllers/Admins/InvoiceController.php
+++ app/Http/Controllers/Admins/InvoiceController.php
@@ -97,9 +97,7 @@ class InvoiceController extends Controller
     public function update(Request $request, $id)
     {
         DB::beginTransaction();
-            $invoice = Invoice::find($id);
-            $invoice_item = $invoice->invoice_items->first();
-            // $invoice_item = InvoiceItem::find($id);
+            $invoice_item = InvoiceItem::find($id);
             $user = User::find($request->get('id'));
 
             if($invoice_item->invoice->warranty_type == 0){
